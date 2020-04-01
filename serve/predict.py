7871cee3871ed2ca71de963ebd94eaae50979b73
minh <minhnguyen2908@gmail.com>
2020-04-01 15:18:10 +0000

    refractor: deploy model for web app

--- serve/predict.py
+++ serve/predict.py
@@ -69,13 +69,13 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
+    
+    data_word_list = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict,data_word_list)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
-    data_pack = np.hstack((data_len, data_X))
+    data_pack = np.hstack((float(data_len), data_X))
     data_pack = data_pack.reshape(1, -1)
     
     data = torch.from_numpy(data_pack)
@@ -87,6 +87,6 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = None
+    result = np.round(model.forward(data).detach().numpy())
 
     return result
